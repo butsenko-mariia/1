538b37dfacba95229258d4df06acdf42636f3f6a
Mariia Butsenko <butsenko.mariia1123@vu.cdu.edu.ua>
2023-11-29 01:31:19 +0200

    Створено найпростіший консольний додаток

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
+            Console.ReadKey();
+        }
+    }
+}
